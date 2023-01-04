9abebf5ce2c3614037deabf0a4065eecb4c5decb
slice_of_code <slice_of_code@mail.ru>
2023-01-04 22:08:16 +0400

    Todolist - step 01

---
+++ react_kabzda_kak_podrobno/src/components/Rating.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Rating() {
+    return <div>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+    </div>
+}
+
+function Star() {
+    return <div>star</div>
+}
+
